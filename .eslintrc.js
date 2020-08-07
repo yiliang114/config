5e71622df596a3f7f4700ebd647e364936b392f2
mrjzhang <mrjzhang@tencent.com>
2020-08-07 15:14:32 +0800

    feat: 添加一条三元运算符的符号位置规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 应对于三元运算符的 = ? :
+    'operator-linebreak': [2, 'after', { overrides: { '?': 'before', ':': 'before', '=': 'before' } }],
+  },
+};
