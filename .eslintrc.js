a85226a2c6de689f389f7a183e1ac0369c79b816
mrjzhang <mrjzhang@tencent.com>
2020-08-07 15:26:49 +0800

    fix: 三元运算符的 = 放在上一行的 end

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // 应对于三元运算符的 = ? :
-    'operator-linebreak': [2, 'after', { overrides: { '?': 'before', ':': 'before', '=': 'before' } }],
+    'operator-linebreak': [2, 'after', { overrides: { '?': 'before', ':': 'before', '=': 'end' } }],
   },
 };
