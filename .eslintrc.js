e03114df4f476b12eb2758aeddfc1da3fb25746f
mrjzhang <mrjzhang@tencent.com>
2020-08-27 22:52:00 +0800

    delete useless files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', 'eslint:recommended'],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     // 应对于三元运算符的 = ? :
-    'operator-linebreak': [2, 'after', { overrides: { '?': 'before', ':': 'before', '=': 'end' } }],
+    "operator-linebreak": [2, "after", { overrides: { "?": "before", ":": "before", "=": "end" } }],
   },
 };
