ea2fcf5c3690799d470acf692758f2777b2d27f2
yiliang114 <1204183885@qq.com>
2022-11-03 01:42:07 +0800

    chore: add commitlint to complete workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    // 应对于三元运算符的 = ? :
-    "operator-linebreak": [2, "after", { overrides: { "?": "before", ":": "before", "=": "end" } }],
+    'no-var': 'error', // 不能使用 var 声明变量
+    'no-extra-semi': 'error',
+    '@typescript-eslint/indent': ['error', 2],
+    'import/extensions': 'off',
+    'linebreak-style': [0, 'error', 'windows'],
+    indent: ['error', 2, { SwitchCase: 1 }], // error类型，缩进2个空格
+    'space-before-function-paren': 0, // 在函数左括号的前面是否有空格
+    'eol-last': 0, // 不检测新文件末尾是否有空行
+    semi: ['error', 'always'], // 在语句后面加分号
+    quotes: ['error', 'single'], // 字符串使用单双引号,double,single
+    'no-console': ['error', { allow: ['log', 'warn'] }], // 允许使用console.log()
+    'arrow-parens': 0,
+    'no-new': 0, //允许使用 new 关键字
+    // 'comma-dangle': [2, 'never'], // 数组和对象键值对最后一个逗号， never参数：不能带末尾的逗号, always参数：必须带末尾的逗号，always-multiline多行模式必须带逗号，单行模式不能带逗号
+    'no-undef': 0,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      modules: true,
+    },
   },
 };
--- README.md
+++ README.md
@@ -5,21 +5,10 @@
 ## workflow
 
 ```bash
-# 1-1 进行了一些开发...
-# 1-2 提交变更集
+# 提交变更集
 pnpm changeset
-# 1-3 提升版本
+# 提升版本
 pnpm version-packages # changeset version
-# 1-4 发包
-pnpm release # pnpm build && pnpm changeset publish --registry=...
-# 1-5 得到 1.0.0-beta.1
-
-# 2-1 进行了一些开发...
-# 2-2 提交变更集
-pnpm changeset
-# 2-3 提升版本
-pnpm version-packages
-# 2-4 发包
+# 发包
 pnpm release
-# 2-5 得到 1.0.0-beta.2
 ```
