c56e6c1a5172345da05591f07fb6f32eaaeb7b49
yiliang114 <1204183885@qq.com>
2023-04-07 00:51:29 +0800

    feat: close #8 eslint 接入 tasks-function, update @yiliang114/eslint-config@2.0.2

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,15 +1,20 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
   env: {
     browser: true,
     es6: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
   },
   extends: [
     'plugin:import/typescript',
@@ -27,18 +32,31 @@ module.exports = {
     'import/resolver': {
       typescript: {
         alwaysTryTypes: true, // always try to resolve types under `<roo/>@types` directory even it doesn't contain any source code, like `@types/unist`
-        directory: process.cwd(),
       },
     },
   },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/camelcase': [
-      'warn',
-      { properties: 'never', ignoreDestructuring: false },
-    ],
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '_' }],
+
+    'import/no-named-default': 'off', // 关闭 import/no-named-default
+    'no-await-in-loop': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-await-in-loop': 'off', // 禁止在循环中出现 await
+    'no-var': 'error', // 不能使用 var 声明变量
+    'no-extra-semi': 'error',
+    'import/extensions': 'off',
+    'linebreak-style': [0, 'error', 'windows'],
+    indent: ['error', 2, { SwitchCase: 1 }], // error类型，缩进 2 个空格
+    'space-before-function-paren': 0, // 在函数左括号的前面是否有空格
+    'eol-last': 0, // 不检测新文件末尾是否有空行
+    semi: ['error', 'always'], // 在语句后面加分号
+    quotes: ['error', 'single', { avoidEscape: true }], // 字符串使用单双引号,double,single
+    'arrow-parens': 0,
+    'no-new': 0, //允许使用 new 关键字
+    // 'comma-dangle': [2, 'never'], // 数组和对象键值对最后一个逗号， never参数：不能带末尾的逗号, always参数：必须带末尾的逗号，always-multiline多行模式必须带逗号，单行模式不能带逗号
+    'no-undef': 0,
   },
 };
