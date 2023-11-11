cee439b41b621bf4d2dd01775d4576806d7434b8
jinjing.zzj <jinjing.zzj@alibaba-inc.com>
2023-11-11 18:19:10 +0800

    fix: opt auto-cr.js

---
+++ script/auto-cr.js
@@ -0,0 +1,3 @@
+const { autoCodeView } = require('../packages/zhinang-cr-bot/dist/index');
+
+autoCodeView(process.PR_NUMBER);
--- script/auto-cr.mjs
+++
@@ -1,3 +0,0 @@
-import { autoCodeView } from '../packages/zhinang-cr-bot/dist/index';
-
-autoCodeView(process.PR_NUMBER);
